32d97997cc59a15fd4d899e3d016db50b37287f9
koki-sato <koki_sato@waku-2.com>
2019-02-03 17:45:32 +0900

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Markdown from 'react-markdown'
 import Layout from '../components/Layout'
+
+const markdown = `
+# Koki Sato Portfolio
+### Links
+  - github: https://github.com/Bernkastel07th
+  - codepen: https://codepen.io/Bernkastel07th/
+  - 当ポートフォリオサイトのソースコード: https://github.com/Bernkastel07th/portfolio
+`
 
 export default () => (
   <Layout>
-    <h1>Welcome</h1>
+    <Markdown source={markdown} />
   </Layout>
 )
